98b5527a09ddaec94b55d6b6178fefe1f8a14797
Ivo te Sligte <ivotesligte@gmail.com>
2026-07-06 21:51:24 +0200

    more precise profiling logs

--- src/linux.rs
+++ src/linux.rs
@@ -14,6 +14,7 @@ fn capture_image(capture: &mut WaylandCapture) -> Yuv420Image {
     }
 }
 
+// TODO: show_cursor: bool parameter
 pub fn capture_video(frame_rate: u64) -> impl Iterator<Item = Yuv420Image> {
     let mut capture =
         WaylandCapture::connect().expect("Cannot connect to Wayland and X11 is not yet supported");
@@ -23,8 +24,8 @@ pub fn capture_video(frame_rate: u64) -> impl Iterator<Item = Yuv420Image> {
     std::iter::from_fn(move || {
         let now_frame = Instant::now();
         debug!(
-            "Time since last frame: {}ms",
-            (now_frame - last).as_millis()
+            "Time since last frame: {:.2}ms",
+            (now_frame - last).as_micros() as f32 / 1000.0
         );
         let diff = now_frame.duration_since(last);
         if diff < frame_duration {
@@ -33,8 +34,8 @@ pub fn capture_video(frame_rate: u64) -> impl Iterator<Item = Yuv420Image> {
         let now_capture = Instant::now();
         let frame = capture_image(&mut capture);
         debug!(
-            "Frame capture time: {}ms",
-            (Instant::now() - now_capture).as_millis()
+            "Frame capture time: {:.2}ms",
+            (Instant::now() - now_capture).as_micros() as f32 / 1000.0
         );
         last = now_frame;
         Some(frame)
